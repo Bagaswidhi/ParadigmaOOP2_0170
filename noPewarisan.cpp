5c0ac08d9d1f2d2afd1c5d890a9f57d8b0fc4292
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-02 15:02:17 +0700

    Mendeklarasikan kelas turunan dari baseclass, error karena baseclass menggunakan final specifier

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -6,3 +6,10 @@ class baseClass final{
         cout << "Hallo saya Function dari base class" << endl;
     }
 };
+
+class derivedClass : public baseClass{
+    public:
+        void perkenalan() {
+            cout << "Hallo saya Function dari derived class" << endl;
+        }
+};
