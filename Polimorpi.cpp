4a100b226415fb436ef81eadbe412cb7e2b53573
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-02 14:36:23 +0700

    Merapikan syntax dengan format documment

--- Polimorpi.cpp
+++ Polimorpi.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 using namespace std;
 
-class seseorang{
-    public:
+class seseorang
+{
+public:
     // virtual void pesan() = 0; // Pure virtual function
-    virtual void pesan() {
+    virtual void pesan()
+    {
         cout << "Pesan dari seseorang" << endl;
     }
 };
 
-class joko : public seseorang{
-    public :
-    void pesan() {
+class joko : public seseorang
+{
+public:
+    void pesan()
+    {
         cout << "Pesan dari joko" << endl;
     }
 };
 
-class lia : public seseorang{
-    public :
-    void pesan() {
+class lia : public seseorang
+{
+public:
+    void pesan()
+    {
         cout << "Pesan dari lia" << endl;
     }
 };
 
-int main(){
-    seseorang* obyek;
+int main()
+{
+    seseorang *obyek;
     joko a;
     lia b;
 
-    obyek = &a; // Memberi nilai objek dengan alamat objek dari class joko
+    obyek = &a;     // Memberi nilai objek dengan alamat objek dari class joko
     obyek->pesan(); // Memanggil fungsi pesan() dari class joko
-    obyek = &b; // Memberi nilai objek dengan alamat objek dari class lia
+    obyek = &b;     // Memberi nilai objek dengan alamat objek dari class lia
     obyek->pesan(); // Memanggil fungsi pesan() dari class lia
-    // b.seseorang::pesan(); // Memanggil fungsi pesan() dari class seseorang 
+    // b.seseorang::pesan(); // Memanggil fungsi pesan() dari class seseorang
     // a.seseorang::pesan(); // Memanggil fungsi pesan() dari class seseorang
     return 0;
 }
